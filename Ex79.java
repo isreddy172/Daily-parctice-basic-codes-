1eb8d4a099b1ed36e057f0e482bd1751068e49c1
Indrasenareddy <121629480+isreddy172@users.noreply.github.com>
2023-08-10 15:53:08 +0530

    Update Ex79.java

--- Ex79.java
+++ Ex79.java
@@ -1,16 +1,12 @@
 //Write a Java program to rotate an array (length 3) of integers in the left direction. 
+
+import java.util.*;
+
 public class Ex79{
     public static void main(String...args)
     {
-       int[] arr = {2,4,5,20};
-       int[] arr1 = new int[arr.length];
-       for(int i=1;i<arr.length-1;i++)
-       {
-        arr1[i]  = arr[arr.length-1];
-        // arr1[i]= arr[i];
-        // arr1[i] = arr[0];
-        System.out.println(arr1[i]);
-       }
-     
+       int[] arr = {2,4,5};
+       int[] c ={arr[arr.length-1],arr[1],arr[0]};
+       System.out.println(Arrays.toString(c));
     }
 }
