7d7fcf4f8f9ebfc64d368ed4f3d461becac97816
isreddy172 <121629480+isreddy172@users.noreply.github.com>
2023-08-14 12:54:36 +0530

    85

---
+++ Ex85.java
@@ -0,0 +1,8 @@
+//Write a Java program to check if a string starts with a specified word. 
+class Ex85 {
+    public static void main(String...args)
+    {
+        String a = "hi hello world";
+       System.out.println(a.startsWith("hello"));
+    }
+}
